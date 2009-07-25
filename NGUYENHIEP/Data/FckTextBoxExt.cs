d8a5201ee39efa76c451dc365548887d86045b8b
duongbkit <duongbkit@9f7f3c9c-53d4-11de-8b28-e1b452efc53a>
2009-07-25 18:09:06 +0000

    +Add FCKEditor

---
+++ NGUYENHIEP/Data/FckTextBoxExt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System.Web.Mvc
+{
+    using System;
+    using System.Globalization;
+    /// <summary>
+    ///  HTMLHelper of Fckeditor
+    /// http://chsword.cnblogs.com/
+    /// </summary>
+    static public class FckTextBoxExt
+    {
+        /// <summary>
+        /// Fckeditor'sHTMLHelper,can be binding with ViewData
+        /// </summary>
+        /// <param name="u">HtmlHelper</param>
+        /// <param name="name">Html tag 's NAME</param>
+        /// <returns></returns>
+        public static string FckTextBox(this HtmlHelper u, string name)
+        {
+            return u.FckTextBox(name, null);
+        }
+        /// <summary>
+        /// Fckeditor'sHTMLHelper
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="name">Html name </param>
+        /// <param name="value">Content</param>
+        /// <returns></returns>
+        public static string FckTextBox(this HtmlHelper u, string name, object value)
+        {
+            return u.FckTextBox(name, value.ToString());
+        }
+        /// <summary>
+        /// Fckeditor’sHTMLHelper
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="name">Html name</param>
+        /// <param name="value">Content</param>
+        /// <returns></returns>
+        public static string FckTextBox(this HtmlHelper u, string name, string value)
+        {
+            if (value == null)
+            {
+                value = Convert.ToString(u.ViewDataContainer.ViewData[name], CultureInfo.InvariantCulture);
+            }
+
+            return string.Format(@"&lt;textarea name=""{0}"" id=""{0}"" rows=""50"" cols=""80"" style=""width:100%; height: 600px""&gt;{1}</textarea>
+<script type=""text/javascript""&gt;
+    var oFCKeditor = new FCKeditor('{0}') ;
+    //oFCKeditor.BasePath    = sBasePath ;
+oFCKeditor.Height=400;
+    oFCKeditor.ReplaceTextarea() ;
+</script>
+", name, value);
+
+        }
+    }
+}
