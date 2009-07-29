29e3141dd3fd3e1794d5be8954ccd6f867a352c5
duongbkit <duongbkit@9f7f3c9c-53d4-11de-8b28-e1b452efc53a>
2009-07-29 06:56:37 +0000

    +Update framework

---
+++ trunk/NGUYENHIEP/Routing/NguyenHiep.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NGUYENHIEP.Routing
+{
+  public class NguyenHiep:BaseRoute
+  {
+    public NguyenHiep(UrlHelper url)
+      : base(url, "NguyenHiep")
+    {
+
+    }
+    public String UrlForIndexForNews()
+    {
+      return UrlFor("ForIndexForNews");
+    }
+    public String UrlForIndexForProduct()
+    {
+      return UrlFor("IndexForProduct");
+    }
+    public String UrlForIndexForIndex()
+    {
+      return UrlFor("Index");
+    }
+    public String UrlForIndexForContruction()
+    {
+      return UrlFor("IndexForContruction");
+    }
+    public String UrlForViewNews(Guid? newsID)
+    {
+      return UrlFor("ViewNews", new { NewsID = newsID });
+    }
+    public String UrlForViewProduct()
+    {
+      return UrlFor("ViewProduct");
+
+    }
+    public String UrlForListAllNews()
+    {
+      return UrlFor("ListAllNews");
+    }
+    public String UrlForListAllContruction()
+    {
+      return UrlFor("ListAllContruction");
+    }
+    public String UrlForListAllProduct()
+    {
+      return UrlFor("ListAllProduct");
+    }
+    public String UrlForListCategory()
+    {
+      return UrlFor("ListCategory");
+    }
+    public String UrlForEditNews(Guid? newsID)
+    {
+      return UrlFor("EditNews", new { NewsID = newsID });
+    }
+    public String UrlForEditCategory()
+    {
+      return UrlFor("EditCategory");
+    }
+    public String UrlForEditProduct()
+    {
+      return UrlFor("EditProduct");
+    }
+    
+
+  }
+}
